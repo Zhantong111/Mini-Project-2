9840fd5fe00b0b978fe2361fb42e6809acfeebdf
Zhan Tong <zhantong94@gmail.com>
2023-03-13 12:31:16 +0800

    hadnleSubmit added

--- src/AddTodo.jsx
+++ src/AddTodo.jsx
@@ -1,12 +1,21 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 function AddTodo() {
+    const [todo, setTodo] = useState("");
+
+    const handleSubmit = () => {
+        console.log(todo)
+    }
+    
     return (
         <div>
-            <input type="text" placeholder="Add a Todo"/>
-            <button type="submit">Submit</button>
+            <input
+                type="text"
+                placeholder="Add a Todo"
+                value={todo}
+                onChange={(e) => setTodo(e.target.value)}/>
+            <button type="submit" onClick={handleSubmit}>Submit</button>
         </div>
     );
 }
-
 export default AddTodo;
