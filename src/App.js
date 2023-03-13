77dbd4effe7dc905076c98576a55bb84e4dfcb68
Zhan Tong <zhantong94@gmail.com>
2023-03-13 17:50:58 +0800

    Extension added

--- src/App.js
+++ src/App.js
@@ -3,12 +3,39 @@ import AddTodo from "./AddTodo";
 import AllTodos from "./AllTodos";
 
 function App() {
+  const [todos, setTodos] = useState([]);
+
   return (
     <>
       <h1>My To Do List</h1>
-      <AddTodo />
-      <AllTodos />
+      <AddTodo setTodos={setTodos} />
+      <AllTodos todos={todos} />
     </>
+  );
+}
+
+function AllTodos({ todos }) {
+  return (
+    <ul>
+      {todos.map((todo) => (
+        <li key={todo}>{todo}</li>
+      ))}
+    </ul>
+  );
+}
+
+function AddTodo({ setTodos }) {
+  function handleSubmit(event) {
+    event.preventDefault();
+    const todo = event.target.elements.todo.value;
+    setTodos((prevTodos) => [...prevTodos, todo]);
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input type="text" id="todo" />
+      <button type="submit">Add Todo</button>
+    </form>
   );
 }
 
